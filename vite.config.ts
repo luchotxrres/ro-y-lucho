998878f4968f9655392f252b2eb58c5e6b3a3fa8
Luciano Torres <luchotxrres@MacBook-Pro-de-Luciano.local>
2026-02-10 13:38:49 -0300

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://luchotxrres.github.io/ro-y-lucho",
+  base: "/ro-y-lucho",
   define: {
     "process.env": {},
   }
